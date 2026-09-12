64dcc2432d85a0bdf64bb706be8787042beb8db6
cinap_lenrek <cinap_lenrek@felloff.net>
2026-09-12 21:52:56 +0000

    gui-x11: send element count, not byte count, in TARGETS reply (thanks red)
    some programs, particularly sdl3-based clients, immediately crash
    whenever anything is snarfed in drawterm.
    
    the reason is that XChangeProperty expects the element count at
    the last argument, but we currently send the byte count of the
    atoms array.

--- gui-x11/x11.c
+++ gui-x11/x11.c
@@ -1202,7 +1202,7 @@ if(0) iprint("xselect target=%d requestor=%d property=%d selection=%d\n",
 		a[3] = compoundtext;
 
 		XChangeProperty(xd, xe->requestor, xe->property, XA_ATOM,
-			32, PropModeReplace, (uchar*)a, sizeof a);
+			32, PropModeReplace, (uchar*)a, nelem(a));
 	}else if(xe->target == XA_STRING || xe->target == utf8string || xe->target == text || xe->target == compoundtext){
 	text:
 		/* if the target is STRING we're supposed to reply with Latin1 XXX */
